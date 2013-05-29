faa63ecb43784e2bf377d9ff36b4bbf6af5cbbc0
trade <trade@ubuntu.(none)>
2013-05-29 14:07:01 +0800

    first commit

---
+++ src/test/java/test/TestTrade.java
@@ -0,0 +1,49 @@
+package test;
+
+import org.bridj.BridJ;
+import org.bridj.Pointer;
+import kstradeapija.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: trade
+ * Date: 13-5-16
+ * Time: 下午10:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestTrade
+{
+    public static void main( String[] argv )
+    {
+        System.out.println( "Start TestTrade------------------------" ) ;
+
+        BridJ.register(CThostFtdcTraderApi.class);    // 必须的
+
+        Pointer<CThostFtdcTraderApi> PointerThostFtdcTraderApi = CThostFtdcTraderApi.CreateFtdcTraderApi( Pointer.pointerToCString( "" ) , false ) ;
+        CThostFtdcTraderApi ftdcTraderApi = PointerThostFtdcTraderApi.get( ) ;
+
+        /**
+         * 如果不加入这段代码，会导致 BridJ类中的public static synchronized Object getJavaObjectFromNativePeer(long peer) {
+         * 获取不到strongNativeObjects的对应对象。
+         */
+        BridJ.protectFromGC( ftdcTraderApi ) ;        // 必须的
+
+        CThostFtdcTraderSpi tradeSpi = new TestTradeSpi( ftdcTraderApi ) ;
+
+        /**
+         * 如果不加入这段代码，会导致 BridJ类中的public static synchronized Object getJavaObjectFromNativePeer(long peer) {
+         * 获取不到strongNativeObjects的对应对象。
+         */
+        BridJ.protectFromGC( tradeSpi ) ;            // 必须的
+
+        ftdcTraderApi.RegisterSpi( Pointer.pointerTo( tradeSpi ) ) ;
+
+        ftdcTraderApi.RegisterFront( Pointer.pointerToCString( "tcp://221.238.214.89:13159" ) );
+        ftdcTraderApi.Init();
+
+        ftdcTraderApi.Join( ) ;
+
+        System.out.println( "End TestTrade------------------------" ) ;
+    }
+
+}
